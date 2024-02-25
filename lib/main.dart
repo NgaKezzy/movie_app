8f150e9dda5137f2a7e074038a61b76f850b2095
ngakezzy@gmail.com <ngakezzy@gmail.com>
2024-02-25 19:19:26 +0700

    thêm font và sửa theme data

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:movie_app/l10n/cubit/locale_cubit.dart';
-import 'package:movie_app/l10n/locale_controller.dart';
+import 'package:movie_app/theme/dark_theme.dart';
+import 'package:movie_app/theme/light_theme.dart';
 
 String language = 'vi';
 void main() {
@@ -25,15 +26,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final LocaleCubit localeCubit = context.watch<LocaleCubit>();
     return MaterialApp(
-      title: 'Flutter Demo',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       locale: Locale(localeCubit.state.languageCode.isEmpty
           ? 'en'
           : localeCubit.state.languageCode),
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+      theme: light,
       home: const MyHomePage(),
     );
   }
@@ -59,16 +57,20 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Text(
               AppLocalizations.of(context)!.helloWorld,
-              style: TextStyle(color: theme.colorScheme.onSurface),
+              style: TextStyle(
+                  color: theme.colorScheme.onSurface,
+                  fontWeight: FontWeight.w600),
             ),
             SizedBox(
               height: 30,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                print(currentBrightness.toString());
+              },
               child: Text(
                 AppLocalizations.of(context)!.changeTheme,
-                style: TextStyle(color: theme.colorScheme.onSurface),
+                style: TextStyle(color: theme.colorScheme.tertiary),
               ),
             ),
             SizedBox(
